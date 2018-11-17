75c1157b0f4249c01ce5a6e425cb61c2781d9a95
thaura <thanos.strantzalis@gmail.com>
2018-11-17 21:27:46 +0100

    remove suspended geodata.gov server
    clean data csv to remove suspended geodata.gov server
    fix big - Greek dataset name did not appear correctly on error messages

--- mainPlugin.py
+++ mainPlugin.py
@@ -45,11 +45,11 @@ sys.setdefaultencoding("utf-8")
 
 
 class GreekOpenData:
-    """QGIS Plugin Implementation."""   
+    """QGIS Plugin Implementation."""
 
-    
+
     def __init__(self, iface):
-    
+
         """Constructor.
 
         :param iface: An interface instance that will be passed to this class
@@ -83,23 +83,23 @@ class GreekOpenData:
         self.language = "GR"  #start language
         self.csvWithDatasets = os.path.join(os.path.dirname(__file__),"data.csv")
         self.quicklooks_dir =  os.path.join(os.path.dirname(__file__),"quicklooks")
-        
+
         # TODO: We are going to let the user set this up in a future iteration
         self.toolbar = self.iface.addToolBar(u'GreekOpenData')
         self.toolbar.setObjectName(u'GreekOpenData')
         # define the datasets used. List of webServices custon objects
-        self.datasets = self.loadDatasets()        
-        #events        
+        self.datasets = self.loadDatasets()
+        #events
         self.dlg.tableWidget.itemSelectionChanged.connect(self.updateDescAndQL)
         self.dlg.load_btn.released.connect(self.loadWebService)
-        self.dlg.close_btn.released.connect(self.close)        
+        self.dlg.close_btn.released.connect(self.close)
         self.init_table()
-        self.updateLanguage()        
-        self.dlg.search_lineEdit.textEdited.connect(self.search)        
+        self.updateLanguage()
+        self.dlg.search_lineEdit.textEdited.connect(self.search)
         self.dlg.language_comboBox.currentIndexChanged.connect(self.updateLanguage)
         self.dlg.info_btn.released.connect(self.showInfo)
-  
-   
+
+
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -185,7 +185,7 @@ class GreekOpenData:
             self.iface.addPluginToWebMenu(
                 self.menu,
                 action)
-            
+
 
         self.actions.append(action)
 
@@ -199,7 +199,7 @@ class GreekOpenData:
             icon_path,
             text=self.tr(u''),
             callback=self.run,
-            parent=self.iface.mainWindow())        
+            parent=self.iface.mainWindow())
 
 
     def unload(self):
@@ -215,7 +215,7 @@ class GreekOpenData:
     def getSelectedNameAndType(self):
         """Gets the name and servive type of the selected dataset."""
         # get the selected row (list with indices)
-        selectedIndexes = self.dlg.tableWidget.selectionModel().selectedRows()        
+        selectedIndexes = self.dlg.tableWidget.selectionModel().selectedRows()
         row = selectedIndexes[0].row()
 
         # get name and service typeof selected row
@@ -227,8 +227,8 @@ class GreekOpenData:
 
     def loadDatasets(self):
         f = open(self.csvWithDatasets, "rb")
-        reader = csv.reader(f)       
-        
+        reader = csv.reader(f)
+
         webServicesList = []
         rownum = 0
         for row in reader:
@@ -250,16 +250,16 @@ class GreekOpenData:
 
                 webServiceObj = WebServiceParams(name, nameGr,nameEn,sourceGR,sourceEN,creationDate,lastUpdate,
                                                  descEN,descGR,serviceType, layerName,server,QLname)
-                         
+
 
                 webServicesList.append(webServiceObj)
             rownum = rownum + 1
-                
+
         f.close()
         return webServicesList
     def updateLanguage(self):
         # get the new language
-        language = str(self.dlg.language_comboBox.currentText())        
+        language = str(self.dlg.language_comboBox.currentText())
         # Change the self.language propertie and change the labe;s
         if language == "English":
             self.language = "EN"
@@ -291,29 +291,29 @@ class GreekOpenData:
             scene = QGraphicsScene()
             pic = QPixmap()
             scene.addItem(QGraphicsPixmapItem(pic))
-            self.dlg.graphicsView.setScene(scene)        
-            self.dlg.graphicsView.show()          
+            self.dlg.graphicsView.setScene(scene)
+            self.dlg.graphicsView.show()
 
-        
-        
+
+
     def updateDescAndQL(self):
         # get the name of the selected dataset
-        dataset_name, dataset_serviceType = self.getSelectedNameAndType()        
+        dataset_name, dataset_serviceType = self.getSelectedNameAndType()
 
         #custom web service object
-        dataset = self.selectdataSets(dataset_name,dataset_serviceType)        
+        dataset = self.selectdataSets(dataset_name,dataset_serviceType)
 
         quicklook = os.path.join(self.quicklooks_dir, dataset.QLname+".jpg")
         desc = dataset.getDescription(self.language)
         name = dataset.getName(self.language)
 
         #update decription
-        self.dlg.textEdit.clear()       
+        self.dlg.textEdit.clear()
         #creation and last update
         if self.language =="EN":
-            crDate = "Creation date : "+dataset.creationDate 
-            update = "Last update : "+dataset.lastUpdate            
-        elif self.language =="GR":            
+            crDate = "Creation date : "+dataset.creationDate
+            update = "Last update : "+dataset.lastUpdate
+        elif self.language =="GR":
             crDate = unicode("Ημερομηνια δημιουργιας : "+dataset.creationDate,'utf-8')
             update = unicode("Τελευταία ενημέρωση : "+dataset.lastUpdate,'utf-8')
 
@@ -324,8 +324,8 @@ class GreekOpenData:
         #cursor.insertHtml("<p><i> "+update+" <br></i></p>")
 
         self.dlg.textEdit.setReadOnly(True)
-        #update quicklook       
-        
+        #update quicklook
+
         #GET DIMENSIONS OF THE IMAGE
         img = Image.open(quicklook)
         w, h = img.size
@@ -336,8 +336,8 @@ class GreekOpenData:
 
         self.dlg.graphicsView.setScene(scene)
         self.dlg.graphicsView.fitInView(QRectF(0, 0, w, h), Qt.KeepAspectRatio)
-        self.dlg.graphicsView.show()        
-        
+        self.dlg.graphicsView.show()
+
 
     def selectdataSets(self, dataset_name, dataset_serviceType):
 
@@ -345,43 +345,43 @@ class GreekOpenData:
             # get the names both in Green and in English in UTF-8 encoding
             dataset_nameGR = unicode(dataset.nameGr, 'utf-8')
             dataset_nameEN = unicode(dataset.nameEn, 'utf-8')
-            # check which dataset has the name we are looking for 
+            # check which dataset has the name we are looking for
             if dataset_name == dataset_nameGR or dataset_name == dataset_nameEN:
                 if dataset_serviceType == dataset.serviceType:
                     selectedDataset = dataset
                     break
-        
+
         return selectedDataset
 
     def setTableWidgetBehavour(self):
 
-        #set rows and columns default sizze and lock it       
+        #set rows and columns default sizze and lock it
 
         self.dlg.tableWidget.setColumnWidth(0,200)
         self.dlg.tableWidget.setColumnWidth(1,45)
         self.dlg.tableWidget.setColumnWidth(2,358)
         self.dlg.tableWidget.horizontalHeader().setResizeMode(QHeaderView.Fixed)
-        self.dlg.tableWidget.verticalHeader().setResizeMode(QHeaderView.Fixed)        
-        
-        self.dlg.tableWidget.resizeRowsToContents() 
-        
+        self.dlg.tableWidget.verticalHeader().setResizeMode(QHeaderView.Fixed)
+
+        self.dlg.tableWidget.resizeRowsToContents()
+
         # qtableWidget behavour
         self.dlg.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.dlg.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        
+
     def fill_table(self,datasetsList):
-         # first delete all elements of table        
+         # first delete all elements of table
         self.dlg.tableWidget.setRowCount(0)
         self.dlg.tableWidget.setColumnCount(3)
 
-                        
+
         #FILL THE TABLE WIDGET
         # get a sorted version of lirt with WebServiceObjects
         WebServiceObjects = self.sort(datasetsList)
-    
+
         for dataset in WebServiceObjects:
             index = WebServiceObjects.index(dataset)
-            self.dlg.tableWidget.insertRow(index)            
+            self.dlg.tableWidget.insertRow(index)
         # fill layer name
             self.dlg.tableWidget.setItem(index , 0, QTableWidgetItem(dataset.getName(self.language))) #dataset name
             self.dlg.tableWidget.setItem(index , 1, QTableWidgetItem(dataset.serviceType)) # webservice type
@@ -396,8 +396,8 @@ class GreekOpenData:
             orggr = unicode("Οργανισμός", 'utf-8')
             self.dlg.tableWidget.setHorizontalHeaderLabels([namegr,typegr,orggr])
 
-        self.setTableWidgetBehavour() 
-        
+        self.setTableWidgetBehavour()
+
 
     def init_table(self):
         # fille the table with the entire dataset collection
@@ -414,8 +414,8 @@ class GreekOpenData:
         elif self.language == "GR":
             self.dlg.search_lineEdit.setText(unicode("Αναζήτηση για όνομα, τύπο υπηρεσίας ή οργανισμό...","utf-8"))
 
-        
-           
+
+
 
     def search(self):
         # first set/change the  font of the serach box
@@ -425,34 +425,34 @@ class GreekOpenData:
 
         # clear description and quicklook
         self.dlg.textEdit.clear()
-        
+
         # make a new emptey scene to show
         if not self.dlg.graphicsView is None:
             scene = QGraphicsScene()
             pic = QPixmap()
             scene.addItem(QGraphicsPixmapItem(pic))
-            self.dlg.graphicsView.setScene(scene)        
-            self.dlg.graphicsView.show()  
-        
+            self.dlg.graphicsView.setScene(scene)
+            self.dlg.graphicsView.show()
+
         # function the searches for a string in the datasets name, service type and otganization
-        text = self.dlg.search_lineEdit.text()        
+        text = self.dlg.search_lineEdit.text()
         # convert to lower case and remove greek accents in case of Greek
         text = text.lower()
-        text = self.removeGreekAccents(text)  
+        text = self.removeGreekAccents(text)
         foundDatasets = []
         for dataset in self.datasets:
             # use lowercase characters and remove greek accents , to make the comparison
             name = self.removeGreekAccents(dataset.getName(self.language).lower())
             source = self.removeGreekAccents(dataset.getSource(self.language).lower())
             serviceType = self.removeGreekAccents(dataset.serviceType.lower())
-            
-            if text in name or text in source or text in serviceType:            
-            #QMessageBox.information(None, "DEBUG:", str(type(dataset.getName(self.language))))                           
+
+            if text in name or text in source or text in serviceType:
+            #QMessageBox.information(None, "DEBUG:", str(type(dataset.getName(self.language))))
                 foundDatasets.append(dataset)
         #fill the table with the found datasets
-        self.fill_table(foundDatasets)     
-            
-        
+        self.fill_table(foundDatasets)
+
+
 
     def sort(self,listOfWebServiceObj):
         """Sorts the datasets by name."""
@@ -462,19 +462,19 @@ class GreekOpenData:
             name = WebServiceObj.getName(self.language)
             # convert to lowercase and remove Greek accents to order properly
             name = name.lower()
-            name = self.removeGreekAccents(name)         
-            
+            name = self.removeGreekAccents(name)
+
             sortedList.append([name,WebServiceObj])
         sortedList.sort()
         #build the output list (only with the WebServiceObj)
         outputList = []
         for element in sortedList:
-            outputList.append(element[1])       
+            outputList.append(element[1])
 
         return outputList
 
     def removeGreekAccents(self,utext):
-        #function to removes the Greek accents from a unicode lowercase string           
+        #function to removes the Greek accents from a unicode lowercase string
         if "ά" in utext:
             #SQMessageBox.information(None, "DEBUG:", str("removeGreekAccents condition reached"))
             utext = utext.replace("ά","α")
@@ -489,32 +489,34 @@ class GreekOpenData:
         if "ύ" in utext:
             utext = utext.replace("ύ","υ")
         if "ώ" in utext:
-            utext = utext.replace("ώ","ω")        
-               
+            utext = utext.replace("ώ","ω")
+
         return utext
-            
-  
+
+
 
     def loadWebService(self):
         # get the selected row
-        dataset_name,dataset_serviceType = self.getSelectedNameAndType()        
+        dataset_name,dataset_serviceType = self.getSelectedNameAndType()
+        #dataset_name = unicode(dataset_name,"utf-8")
 
         #custom web service object
-        dataset = self.selectdataSets(dataset_name,dataset_serviceType)        
-                
-        urlWithParams = dataset.webServiceParams()        
+        dataset = self.selectdataSets(dataset_name,dataset_serviceType)
+
+        urlWithParams = dataset.webServiceParams()
         if dataset.serviceType== "WMS":
-            rlayer = QgsRasterLayer(dataset.webServiceParams(), dataset.getName(self.language), dataset.serviceType.lower())            
+            rlayer = QgsRasterLayer(dataset.webServiceParams(), dataset.getName(self.language), dataset.serviceType.lower())
             if not rlayer.isValid():
-                QMessageBox.information(None, "ERROR:", str("..Oops! " + dataset_name + "  cannot be loaded. Either the server is down or you have limited internet connectivity"))
-               
-            QgsMapLayerRegistry.instance().addMapLayer(rlayer)                        
+                #QMessageBox.information(None, "ERROR:", u"..Oops! " + unicode(dataset_name,"utf-8")+ "  cannot be loaded. Either the server is down or you have limited internet connectivity")
+                QMessageBox.information(None, "ERROR:", u"..Oops! {} cannot be loaded. Either the server is down or you have limited internet connectivity".format(dataset_name))
+
+            QgsMapLayerRegistry.instance().addMapLayer(rlayer)
             self.iface.legendInterface().setLayerExpanded(rlayer,False) # collapse the layer'slegend
-        elif dataset.serviceType== "WFS":                       
+        elif dataset.serviceType== "WFS":
             vlayer = QgsVectorLayer(dataset.webServiceParams(), dataset.getName(self.language), dataset.serviceType)
-            #QMessageBox.information(None, "ERROR:", str(dataset.webServiceParams())) 
+            #QMessageBox.information(None, "ERROR:", str(dataset.webServiceParams()))
             if not vlayer.isValid():
-                QMessageBox.information(None, "ERROR:", str("..Oops! " + dataset_name + "  cannot be loaded. Either the server is down or you have limited internet connectivity"))
+                QMessageBox.information(None, "ERROR:", u"..Oops! {} cannot be loaded. Either the server is down or you have limited internet connectivity".format(dataset_name))
             QgsMapLayerRegistry.instance().addMapLayer(vlayer)
             #re-appear window
             self.dlg.raise_()
@@ -526,13 +528,13 @@ class GreekOpenData:
         elif self.language == "GR":
             info_html = "info_gr.html"
         # show htmpl in browser
-        
-        local_path = os.path.join(self.plugin_dir,info_html)        
-        abs_path = os.path.abspath(local_path)       
-        url = urlparse.urljoin('file:', urllib.pathname2url(abs_path))         
+
+        local_path = os.path.join(self.plugin_dir,info_html)
+        abs_path = os.path.abspath(local_path)
+        url = urlparse.urljoin('file:', urllib.pathname2url(abs_path))
         #QMessageBox.information(None, "ERROR:", str(url))
         webbrowser.open_new_tab(url)
-                
+
     def run(self):
         """Run method that performs all the real work"""
         # show the dialog
@@ -558,7 +560,7 @@ class GreekOpenData:
             scene = QGraphicsScene()
             pic = QPixmap()
             scene.addItem(QGraphicsPixmapItem(pic))
-            self.dlg.graphicsView.setScene(scene)        
+            self.dlg.graphicsView.setScene(scene)
             self.dlg.graphicsView.show()
         """close the dialog"""
         self.dlg.close()
